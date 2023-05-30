a1cca9eda8deb9f2abc1a2048c31a3336c322c46
RuanHeleno <ruan.empreenderop@gmail.com>
2023-05-30 18:59:00 -0300

    Resolução do exercício Typescript II

--- src/database.ts
+++ src/database.ts
@@ -6,30 +6,79 @@ export const users: Array<TUser> = [
     name: "Fulano",
     email: "fulano@gmail.com",
     password: "fulano123",
-    createdAt: new Date().toISOString()
+    createdAt: new Date().toISOString(),
   },
   {
     id: "u002",
     name: "Beltrana",
     email: "beltrana@gmail.com",
     password: "beltrana00",
-    createdAt: new Date().toISOString()
-  }
-]
+    createdAt: new Date().toISOString(),
+  },
+];
 
-export const products: Array<TProducts> = [ 
+export const createUser = (
+  id: string,
+  name: string,
+  email: string,
+  password: string
+) => {
+  const newUser: TUser = {
+    id,
+    name,
+    email,
+    password,
+    createdAt: new Date().toISOString(),
+  };
+
+  users.push(newUser);
+  console.log("Cadastro realizado com sucesso");
+};
+
+export const getAllUsers = () => {
+  console.table(users);
+};
+
+export const products: Array<TProducts> = [
   {
     id: "prod001",
     name: "Mouse gamer",
     price: 250,
     description: "Melhor mouse do mercado!",
-    imageUrl: "https://picsum.photos/seed/Mouse%20gamer/400"
+    imageUrl: "https://picsum.photos/seed/Mouse%20gamer/400",
   },
   {
     id: "prod002",
     name: "Monitor",
     price: 900,
     description: "Monitor LED Full HD 24 polegadas",
-    imageUrl: "https://picsum.photos/seed/Monitor/400"
-  }
-]
+    imageUrl: "https://picsum.photos/seed/Monitor/400",
+  },
+];
+
+export const createProduct = (
+  id: string,
+  name: string,
+  price: number,
+  description: string,
+  imageUrl: string
+) => {
+  const newProduct: TProducts = {
+    id,
+    name,
+    price,
+    description,
+    imageUrl
+  };
+
+  products.push(newProduct);
+  console.log("Produto criado com sucesso");
+};
+
+export const getAllProducts = () => {
+  console.table(products);
+};
+
+export const searchProductsByName = (name: string) => { 
+  return products.find(product => product.name.toLowerCase().includes(name.toLowerCase()));
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
-import { products, users } from "./database";
+/* import { products, users } from "./database";
 
 console.log(users);
-console.log(products);
+console.log(products); */
+
+/* import { createUser, getAllUsers } from "./database";
+
+createUser("u003", "RuanHeleno", "ruanheleno@gmail.com", "123");
+getAllUsers(); */
+
+import { createProduct, getAllProducts, searchProductsByName } from "./database";
+
+createProduct("prod003", "Teclado Gamer", 230, "Melhor teclado do mundo!", "https://picsum.photos/seed/Teclado%20gamer/400");
+getAllProducts();
+console.table(searchProductsByName("gamer"));
