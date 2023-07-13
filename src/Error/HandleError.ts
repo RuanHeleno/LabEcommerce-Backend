edb2d6504ed1fd88a6b92e535efcebc9c3df42c8
RuanHeleno <ruan.empreenderop@gmail.com>
2023-07-13 15:06:42 -0300

    Atualizado Handle Error

--- src/Error/HandleError.ts
+++ src/Error/HandleError.ts
@@ -1,8 +1,6 @@
 import { Response } from "express";
 
 export const HandleError = (error: Error, res: Response): void => {
-  console.log(error);
-
   if (res.statusCode === 200) res.status(500);
 
   res.send(error.message);
